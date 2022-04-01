5f7ad32c9733bb7d729730c9e2cf64a635649030
Zhicheng Geng <zhichenggeng@utexas.edu>
2022-03-31 19:30:29 -0500

    Add requirements.txt

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -37,30 +37,6 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam([torch.zeros(3, 64, 3, 3)], lr=2e-4, weight_decay=0,
                                  betas=(0.9, 0.99))
     ##############################
-    # MultiStepLR_Restart
-    ##############################
-    ## Original
-    lr_steps = [200000, 400000, 600000, 800000]
-    restarts = None
-    restart_weights = None
-
-    ## two
-    lr_steps = [100000, 200000, 300000, 400000, 490000, 600000, 700000, 800000, 900000, 990000]
-    restarts = [500000]
-    restart_weights = [1]
-
-    ## four
-    lr_steps = [
-        50000, 100000, 150000, 200000, 240000, 300000, 350000, 400000, 450000, 490000, 550000,
-        600000, 650000, 700000, 740000, 800000, 850000, 900000, 950000, 990000
-    ]
-    restarts = [250000, 500000, 750000]
-    restart_weights = [1, 1, 1]
-
-    scheduler = MultiStepLR_Restart(optimizer, lr_steps, restarts, restart_weights, gamma=0.5,
-                                    clear_state=False)
-
-    ##############################
     # Cosine Annealing Restart
     ##############################
     ## two
@@ -90,9 +66,7 @@ if __name__ == "__main__":
     from matplotlib import pyplot as plt
     import matplotlib.ticker as mtick
     mpl.style.use('default')
-    import seaborn
-    seaborn.set(style='whitegrid')
-    seaborn.set_context('paper')
+    plt.grid('on')
 
     plt.figure(1)
     plt.subplot(111)
